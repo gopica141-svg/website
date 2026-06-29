451f6669cbaea4a52ce9a28ce390e770896becfb
gopica141-svg <gopica141@gmail.com>
2026-06-29 15:36:30 +0530

    Update MongoDB to Atlas

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/cust_data")
+mongoose.connect("mongodb+srv://gopica141_db_user:gopica2005@cluster0.unbz6wi.mongodb.net/cust_data")
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
